d5a0d5baabed61682d5e12dd9f15e912e93df859
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2023-09-06 23:48:13 +0300

    Update Test intersection

--- modules/intersection_tester/__init__.py
+++ modules/intersection_tester/__init__.py
@@ -1,11 +1,16 @@
 # TODO: Сделать быстрое создание лоу-поли для мема со скелетом
 # TODO: Быстрое создание иерархий коллекций
 import math
+from typing import List, Any
+
+import numpy
 
 import bpy
 from bpy.types import Context
 from ...base_panel import BasePanel
 import mathutils
+
+cash = {}
 
 
 def intersect(a, b):
@@ -20,12 +25,15 @@ def intersect(a, b):
 
 
 class Bounding:
-    maxX = -math.inf
-    minX = math.inf
-    maxY = -math.inf
-    minY = math.inf
-    maxZ = -math.inf
-    minZ = math.inf
+    __slots__ = ['maxX', 'minX', 'maxY', 'minY', 'maxZ', 'minZ']
+
+    def __init__(self):
+        self.maxX = -math.inf  # 0
+        self.minX = math.inf  # 1
+        self.maxY = -math.inf  # 2
+        self.minY = math.inf  # 3
+        self.maxZ = -math.inf  # 4
+        self.minZ = math.inf  # 5
 
 
 def to_normal_bounding_box(bounding) -> Bounding:
@@ -53,11 +61,21 @@ def to_normal_bounding_box(bounding) -> Bounding:
     return bound
 
 
-def get_object_bounding(obj: bpy.types.Object):
-    bounding = [obj.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in
-                obj.bound_box[:]]
+def get_object_bounding_same_bounds(obj: bpy.types.Object, bounds):
+    global bounding__
 
-    return to_normal_bounding_box(bounding)
+    if cash.get(obj.name) is None:
+        bounding = [obj.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in bounds]
+        cash[obj.name] = to_normal_bounding_box(bounding)
+    return cash[obj.name]
+
+
+def get_object_bounding(obj: bpy.types.Object):
+    if cash.get(obj.name) is None:
+        bounding = [obj.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in obj.bound_box[:]]
+
+        cash[obj.name] = to_normal_bounding_box(bounding)
+    return cash[obj.name]
 
 
 class ZENU_OT_test_intersection(bpy.types.Operator):
@@ -70,6 +88,7 @@ class ZENU_OT_test_intersection(bpy.types.Operator):
         return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
 
     def execute(self, context: Context):
+        cash.clear()
         test_bounding = get_object_bounding(context.object)
 
         for i in bpy.data.objects:
@@ -91,13 +110,40 @@ class ZENU_OT_test_intersection_all(bpy.types.Operator):
         return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
 
     def execute(self, context: Context):
+        cash.clear()
         bpy.ops.object.select_all(action='DESELECT')
 
         for i in bpy.data.objects:
+            bounding = get_object_bounding(i)
             for j in bpy.data.objects:
                 if j.name == i.name: continue
 
-                if intersect(get_object_bounding(j), get_object_bounding(i)):
+                if intersect(get_object_bounding(j), bounding):
+                    j.select_set(True)
+
+        return {'FINISHED'}
+
+
+class ZENU_OT_test_intersection_all_same_bounds(bpy.types.Operator):
+    bl_label = 'Test Intersection All Same Bounds Opt'
+    bl_idname = 'zenu.test_intersection_all_same_bounds'
+    bl_options = {"UNDO"}
+
+    @classmethod
+    def poll(cls, context: 'Context'):
+        return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
+
+    def execute(self, context: Context):
+        cash.clear()
+        bpy.ops.object.select_all(action='DESELECT')
+        bound = context.object.bound_box
+
+        for i in bpy.data.objects:
+            bounding = get_object_bounding(i)
+            for j in bpy.data.objects:
+                if j.name == i.name: continue
+
+                if intersect(get_object_bounding_same_bounds(j, bound), bounding):
                     j.select_set(True)
 
         return {'FINISHED'}
@@ -113,6 +159,8 @@ class ZENU_OT_test_intersection_in_selection(bpy.types.Operator):
         return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
 
     def execute(self, context: Context):
+        cash.clear()
+
         arr = []
         for i in context.selected_objects:
             for j in context.selected_objects:
@@ -128,6 +176,7 @@ class ZENU_OT_test_intersection_in_selection(bpy.types.Operator):
 
         return {'FINISHED'}
 
+
 class ZENU_PT_test_intersection(BasePanel):
     bl_label = 'Test Intersection'
 
@@ -136,11 +185,13 @@ class ZENU_PT_test_intersection(BasePanel):
         layout.operator(ZENU_OT_test_intersection.bl_idname)
         layout.operator(ZENU_OT_test_intersection_all.bl_idname)
         layout.operator(ZENU_OT_test_intersection_in_selection.bl_idname)
+        layout.operator(ZENU_OT_test_intersection_all_same_bounds.bl_idname)
 
 
 reg, unreg = bpy.utils.register_classes_factory((
     ZENU_OT_test_intersection,
     ZENU_OT_test_intersection_all,
+    ZENU_OT_test_intersection_all_same_bounds,
     ZENU_OT_test_intersection_in_selection,
     ZENU_PT_test_intersection
 ))
