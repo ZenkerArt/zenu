47555de46716b77f557bcd2b872d5096b9ba3b4f
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2023-09-06 21:36:06 +0300

    Update Test intersection

--- modules/intersection_tester/__init__.py
+++ modules/intersection_tester/__init__.py
@@ -53,8 +53,15 @@ def to_normal_bounding_box(bounding) -> Bounding:
     return bound
 
 
+def get_object_bounding(obj: bpy.types.Object):
+    bounding = [obj.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in
+                obj.bound_box[:]]
+
+    return to_normal_bounding_box(bounding)
+
+
 class ZENU_OT_test_intersection(bpy.types.Operator):
-    bl_label = 'Test Intersection'
+    bl_label = 'Select from intersection'
     bl_idname = 'zenu.test_intersection'
     bl_options = {"UNDO"}
 
@@ -63,32 +70,76 @@ class ZENU_OT_test_intersection(bpy.types.Operator):
         return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
 
     def execute(self, context: Context):
-        bounding = [bpy.context.object.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in
-                    bpy.context.object.bound_box[:]]
-        test_bounding = to_normal_bounding_box(bounding)
+        test_bounding = get_object_bounding(context.object)
 
         for i in bpy.data.objects:
             if i.name == context.object.name: continue
-            bounding = [i.matrix_world @ mathutils.Vector(bbox_co[:]) for bbox_co in
-                        i.bound_box[:]]
 
-            if intersect(test_bounding, to_normal_bounding_box(bounding)):
+            if intersect(test_bounding, get_object_bounding(i)):
                 i.select_set(True)
 
         return {'FINISHED'}
 
+
+class ZENU_OT_test_intersection_all(bpy.types.Operator):
+    bl_label = 'Test Intersection All'
+    bl_idname = 'zenu.test_intersection_all'
+    bl_options = {"UNDO"}
+
+    @classmethod
+    def poll(cls, context: 'Context'):
+        return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
+
+    def execute(self, context: Context):
+        for i in bpy.data.objects:
+            for j in bpy.data.objects:
+                if j.name == i.name: continue
+
+                if intersect(get_object_bounding(j), get_object_bounding(i)):
+                    j.select_set(True)
+
+        return {'FINISHED'}
+
+
+class ZENU_OT_test_intersection_in_selection(bpy.types.Operator):
+    bl_label = 'Test Intersection In Selection'
+    bl_idname = 'zenu.test_intersection_in_selection'
+    bl_options = {"UNDO"}
+
+    @classmethod
+    def poll(cls, context: 'Context'):
+        return context.active_object and isinstance(context.active_object.data, bpy.types.Mesh)
+
+    def execute(self, context: Context):
+        arr = []
+        for i in context.selected_objects:
+            for j in context.selected_objects:
+                if j.name == i.name: continue
+
+                if intersect(get_object_bounding(j), get_object_bounding(i)):
+                    arr.append(j)
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        for obj in arr:
+            obj.select_set(True)
+
+        return {'FINISHED'}
 
 class ZENU_PT_test_intersection(BasePanel):
     bl_label = 'Test Intersection'
 
     def draw(self, context: Context):
         layout = self.layout
-        col = layout.split(align=True)
-        col.operator(ZENU_OT_test_intersection.bl_idname)
+        layout.operator(ZENU_OT_test_intersection.bl_idname)
+        layout.operator(ZENU_OT_test_intersection_all.bl_idname)
+        layout.operator(ZENU_OT_test_intersection_in_selection.bl_idname)
 
 
 reg, unreg = bpy.utils.register_classes_factory((
     ZENU_OT_test_intersection,
+    ZENU_OT_test_intersection_all,
+    ZENU_OT_test_intersection_in_selection,
     ZENU_PT_test_intersection
 ))
 
