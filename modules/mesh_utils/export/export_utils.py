3fb78f0b91b94814bef0cbe73dce2bc07e6aaa90
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2024-03-06 14:51:18 +0300

    Add OBJ

--- modules/mesh_utils/export/export_utils.py
+++ modules/mesh_utils/export/export_utils.py
@@ -8,14 +8,34 @@ from bpy_extras.io_utils import ExportHelper
 class BaseExport(bpy.types.Operator, ExportHelper):
     export_name: bpy.props.StringProperty(
         name='Export Name Template',
-        default='$_export.fbx'
+        default='$_export'
     )
+    export_type: bpy.props.EnumProperty(
+        name='Exporter',
+        items=(
+            ('FBX', 'FBX', ''),
+            ('OBJ', 'OBJ', ''),
+        ))
     filepath: bpy.props.StringProperty(
         name="File Path",
         description="Filepath used for exporting the file",
         maxlen=1024,
         subtype='DIR_PATH',
     )
+    export_material: bpy.props.BoolProperty(name='Export Material', default=False)
+    export_color: bpy.props.BoolProperty(name='Export Color', default=True)
+    filename_ext = ''
+
+    def draw(self, context: bpy.types.Context):
+        layout = self.layout
+
+        layout.prop(self, 'export_name')
+        layout.prop(self, 'export_type')
+
+        if self.export_type == 'OBJ':
+            layout.prop(self, 'export_material')
+            layout.prop(self, 'export_color')
+
 
     def invoke(self, context, _event):
         import os
@@ -28,16 +48,20 @@ class BaseExport(bpy.types.Operator, ExportHelper):
     def export(self, objs: list[bpy.types.Object], name: str):
         with bpy.context.temp_override(selected_objects=objs):
             path = os.path.join(os.path.dirname(self.filepath), self.export_name.replace('$', name))
-            bpy.ops.export_scene.fbx(filepath=path, use_selection=True)
+
+            if self.export_type == 'FBX':
+                bpy.ops.export_scene.fbx(filepath=path + '.fbx', use_selection=True)
+            elif self.export_type == 'OBJ':
+                bpy.ops.wm.obj_export(filepath=path + '.obj',
+                                      export_selected_objects=True,
+                                      export_materials=self.export_material,
+                                      export_colors=self.export_color
+                                      )
 
 
 class ZENU_OT_export_by_name(BaseExport):
     bl_label = 'Export By Name'
     bl_idname = 'zenu.export_by_name'
-
-
-
-    filename_ext = ".fbx"
 
     def execute(self, context: bpy.types.Context):
         for obj in context.selected_objects:
@@ -48,8 +72,6 @@ class ZENU_OT_export_by_name(BaseExport):
 class ZENU_OT_export_by_collection(BaseExport):
     bl_label = 'Export By Collection'
     bl_idname = 'zenu.export_by_collection'
-
-    filename_ext = ".fbx"
 
     def execute(self, context: bpy.types.Context):
         objs = defaultdict(list)
