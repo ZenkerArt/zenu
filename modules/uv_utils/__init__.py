52b69b3bce52ab8d2ee05a6354273e9a527cd956
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2024-05-17 13:22:31 +0300

    Uv Update

--- modules/uv_utils/__init__.py
+++ modules/uv_utils/__init__.py
@@ -23,6 +23,42 @@ class ZENU_OT_rotate_selected_uv(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class ZENU_OT_move_uv_by_pixel(bpy.types.Operator):
+    bl_label = 'Move'
+    bl_idname = 'zenu.move_by_pixel'
+    bl_options = {'UNDO'}
+    add: bpy.props.BoolProperty(default=True)
+    step: bpy.props.StringProperty()
+
+    def select_layout(self, context: bpy.types.Context, mesh_origin: bpy.types.Mesh):
+        resolution = Vector((context.scene.zenu_uv_resolution_x, context.scene.zenu_uv_resolution_y))
+        offset = Vector((context.scene.zenu_uv_offset_x / resolution.x, context.scene.zenu_uv_offset_x / resolution.y))
+
+        move = Vector((0, 0, 0))
+
+        if self.step == 'left':
+            move.x = -offset.x
+        if self.step == 'right':
+            move.x = offset.x
+
+        if self.step == 'up':
+            move.y = offset.y
+        if self.step == 'down':
+            move.y = -offset.y
+
+        if not self.add:
+            move = -move
+
+        bpy.ops.transform.translate(value=move, orient_type='GLOBAL',
+                                    orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)))
+
+    def execute(self, context: bpy.types.Context):
+        for obj in context.selected_objects:
+            self.select_layout(context, obj.data)
+
+        return {'FINISHED'}
+
+
 class ZENU_OT_select_uv_shell(bpy.types.Operator):
     bl_label = 'Select UV Shell'
     bl_idname = 'zenu.select_uv_shell'
@@ -81,6 +117,18 @@ class ZENU_OT_select_uv_shell(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class ZENU_OT_select_uv_res(bpy.types.Operator):
+    bl_label = 'Select UV Res'
+    bl_idname = 'zenu.select_uv_res'
+    x: bpy.props.IntProperty()
+    y: bpy.props.IntProperty()
+
+    def execute(self, context: bpy.types.Context):
+        context.scene.zenu_uv_resolution_x = self.x
+        context.scene.zenu_uv_resolution_y = self.y
+        return {'FINISHED'}
+
+
 class ZENU_PT_uv_utils(BasePanel):
     bl_label = 'UV Utils'
     bl_space_type = 'IMAGE_EDITOR'
@@ -109,15 +157,75 @@ class ZENU_PT_uv_utils(BasePanel):
         op = row.operator(ZENU_OT_rotate_selected_uv.bl_idname, text='90°')
         op.angle = 1.5708
 
+        col = layout.column(align=True)
+        col.scale_y = 1.4
+        row = col.row(align=True)
+        op = row.operator(ZENU_OT_select_uv_res.bl_idname, text='4K')
+        op.x = 4096
+        op.y = 4096
+        op = row.operator(ZENU_OT_select_uv_res.bl_idname, text='2K')
+        op.x = 2048
+        op.y = 2048
+        op = row.operator(ZENU_OT_select_uv_res.bl_idname, text='1K')
+        op.x = 1024
+        op.y = 1024
+
+        col.prop(context.scene, 'zenu_uv_resolution_x')
+        col.prop(context.scene, 'zenu_uv_resolution_y')
+        col.prop(context.scene, 'zenu_uv_offset_x', text='Step')
+        # col.prop(context.scene, 'zenu_uv_offset_y')
+
+        # op = col.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='Add')
+        # op.add = True
+        # op = col.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='Subtract')
+        # op.add = False
+
+        move = layout.column(align=True)
+        op = move.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='', icon='TRIA_UP')
+        op.step = 'up'
+        row = move.row(align=True)
+        row.scale_x = 100
+        op = row.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='', icon='TRIA_LEFT')
+        op.step = 'left'
+        op = row.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='', icon='TRIA_RIGHT')
+        op.step = 'right'
+        op = move.operator(ZENU_OT_move_uv_by_pixel.bl_idname, text='', icon='TRIA_DOWN')
+        op.step = 'down'
+
 
 reg, unreg = bpy.utils.register_classes_factory((
     ZENU_PT_uv_utils,
     ZENU_OT_select_uv_shell,
-    ZENU_OT_rotate_selected_uv
+    ZENU_OT_rotate_selected_uv,
+    ZENU_OT_move_uv_by_pixel,
+    ZENU_OT_select_uv_res
 ))
 
 
 def register():
+    bpy.types.Scene.zenu_uv_resolution_x = bpy.props.IntProperty(
+        name='Resolution X',
+        default=1024,
+        min=2,
+        subtype='PIXEL'
+    )
+    bpy.types.Scene.zenu_uv_resolution_y = bpy.props.IntProperty(
+        name='Resolution Y',
+        default=1024,
+        min=2,
+        subtype='PIXEL'
+    )
+    bpy.types.Scene.zenu_uv_offset_x = bpy.props.IntProperty(
+        name='Offset X',
+        default=0,
+        subtype='PIXEL'
+    )
+    bpy.types.Scene.zenu_uv_offset_y = bpy.props.IntProperty(
+        name='Offset Y',
+        default=0,
+        subtype='PIXEL'
+    )
+
     bpy.types.Scene.zenu_uv_aspect_threshold = bpy.props.FloatProperty(
         name='Threshold',
         default=1.1,
