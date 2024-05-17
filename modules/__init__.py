00fde2aa75c680332746786065b6a46e728498d4
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2024-05-17 13:52:03 +0300

    Update

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,7 +4,7 @@ from . import timemarkers, \
     context_pie, shape_key_bind, physic, \
     mesh_utils, menu_manager, \
     hair_physic_settings, ik_switcher, shape_utils, bone_layers, \
-    uv_utils, animation_utils, search, rig, constraint_utils, asset_browser_utils, test_node_tree, tests
+    uv_utils, animation_utils, search, rig, constraint_utils, asset_browser_utils
 
 modules = (
     uv_utils,
@@ -23,8 +23,8 @@ modules = (
     # ik_switcher,
     bone_layers,
     search,
-    rig,
+    # rig,
     asset_browser_utils,
     # test_node_tree,
-    tests
+    # tests
 )
