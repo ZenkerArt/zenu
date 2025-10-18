3931c3548628c415652b9d00dec3d057b8310c45
Zenker <zenkerart@gmail.com>
2025-10-18 11:29:54 +0300

    Update

--- modules/action_selector/__init__.py
+++ modules/action_selector/__init__.py
@@ -4,9 +4,24 @@ from ...base_panel import BasePanel
 import bpy
 
 
+def add_empty(name: str):
+    empty = bpy.data.objects.get(name)
+
+    if empty:
+        return empty
+
+    empty = bpy.data.objects.new(name, None)
+    bpy.context.view_layer.layer_collection.collection.objects.link(empty)
+
+    return empty
+
+
 class AC_Action(bpy.types.PropertyGroup):
     name: str
     action: bpy.props.PointerProperty(type=bpy.types.Action)
+    start: bpy.props.IntProperty()
+    end: bpy.props.IntProperty()
+    isolate: bpy.props.BoolProperty()
 
 
 class ZENU_UL_action_selector(bpy.types.UIList):
@@ -35,7 +50,6 @@ class ZENU_OT_clear_nla(bpy.types.Operator):
                 obj.animation_data_clear()
                 continue
 
-        # print(context.)
         return {'FINISHED'}
 
 
@@ -112,26 +126,19 @@ class ZENU_OT_copy_nla_strip_settings(bpy.types.Operator):
 
     def copy(self, strip: bpy.types.NlaStrip, active_strip: bpy.types.NlaStrip, name: str):
         for active_curve in active_strip.fcurves:
-            # print(active_curve.data_path )
             if active_curve.data_path != name:
-                    continue
-            
+                continue
+
             for copy_curve in strip.fcurves:
                 if copy_curve.data_path != name:
                     continue
 
                 copy_curve.keyframe_points.clear()
-                for index, key_active in enumerate(active_curve.keyframe_points):
-                    strip.keyframe_insert(
-                        data_path=copy_curve.data_path, frame=key_active.co.y)
+                copy_curve.keyframe_points.add(len(active_curve.keyframe_points))
 
                 for index, key_active in enumerate(active_curve.keyframe_points):
-                    # strip.keyframe_insert(
-                        # data_path=copy_curve.data_path, frame=key_active.co.y)
-
                     k = copy_curve.keyframe_points[index]
                     k.co = key_active.co
-                    # print(key_active.co)
 
                     k.handle_left_type = key_active.handle_left_type
                     k.handle_right_type = key_active.handle_right_type
@@ -182,7 +189,7 @@ class ZENU_OT_copy_nla_strip_settings(bpy.types.Operator):
                 strip.use_animated_influence = active_strip.use_animated_influence
                 strip.repeat = active_strip.repeat
                 strip.scale = active_strip.scale
-                
+
                 self.copy(strip, active_strip, 'strip_time')
                 self.copy(strip, active_strip, 'influence')
 
@@ -198,15 +205,19 @@ class ZENU_OT_action_selector_item_operations(bpy.types.Operator):
     action: bpy.props.EnumProperty(items=(
         ('SELECT', 'Select', ''),
         ('RENAME_TO_ACTION_NAME', 'Rename To Action Name', ''),
+        ('ISOLATE', 'Isolate With Action', ''),
     ))
 
-    def select(self, result: set[ActionSearchResult]):
+    active_action: AC_Action = None
+
+    def unhide(self, result: set[ActionSearchResult]):
+        empty = add_empty('Select')
+        bpy.context.view_layer.objects.active = empty
         bpy.ops.object.mode_set(mode='OBJECT')
         bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects.remove(empty)
 
         for find in result:
-            # find.obj.users_collection
-
             try:
                 find.obj.hide_viewport = False
                 find.obj.hide_set(False)
@@ -220,6 +231,11 @@ class ZENU_OT_action_selector_item_operations(bpy.types.Operator):
                 find.obj.hide_set(False)
                 find.obj.select_set(True)
 
+    def select(self, result: set[ActionSearchResult]):
+
+        self.unhide(result)
+
+        for find in result:
             for track in find.obj.animation_data.nla_tracks:
                 for strip in track.strips:
                     strip.select = False
@@ -229,6 +245,50 @@ class ZENU_OT_action_selector_item_operations(bpy.types.Operator):
 
         if result:
             bpy.context.view_layer.objects.active = result.pop().obj
+
+    def isolate(self, result: set[ActionSearchResult]):
+        self.unhide(result)
+
+        for r in result:
+            strips: list[bpy.types.NlaStrip] = []
+
+            for track in r.obj.animation_data.nla_tracks:
+                for strip in track.strips:
+                    strips.append(strip)
+
+            if r.obj.zenu_is_isolate:
+                bpy.context.scene.use_preview_range = False
+                r.obj.zenu_is_isolate = False
+                r.obj.animation_data.action = r.obj['zenu_isolate_changes']['action']
+                for strip in strips:
+                    val = r.obj['zenu_isolate_changes']['changes'].get(
+                        strip.name)
+
+                    if val is None:
+                        continue
+
+                    strip.mute = val
+                continue
+
+            changes = {}
+            for strip in strips:
+                if not strip.mute:
+                    changes[strip.name] = False
+                    strip.mute = True
+
+            r.obj['zenu_isolate_changes'] = {
+                'changes': changes,
+                'action': r.obj.animation_data.action
+            }
+            r.obj.animation_data.action = self.active_action.action
+            r.obj.zenu_is_isolate = True
+            
+            if self.active_action.start == self.active_action.end:
+                continue
+            
+            bpy.context.scene.frame_preview_end = self.active_action.end
+            bpy.context.scene.frame_preview_start = self.active_action.start
+            bpy.context.scene.use_preview_range = True
 
     def execute(self, context: bpy.types.Context):
         item: AC_Action = None
@@ -244,10 +304,15 @@ class ZENU_OT_action_selector_item_operations(bpy.types.Operator):
         if item is None:
             return {'FINISHED'}
 
+        self.active_action = item
+
         result = find_all_object_with_action_name(item.action.name)
+        
 
         if self.action == 'SELECT':
             self.select(result)
+        elif self.action == 'ISOLATE':
+            self.isolate(result)
         elif self.action == 'RENAME_TO_ACTION_NAME':
             for find in result:
                 for strip in find.strips:
@@ -259,6 +324,13 @@ class ZENU_OT_action_selector_item_operations(bpy.types.Operator):
 class ZENU_PT_action_selector(BasePanel):
     bl_label = "Action Selector"
     bl_context = ''
+    action_name: str = ''
+
+    def draw_set_op(self, layout: bpy.types.UILayout, name: str, icon: str, action: str, active: bool = False):
+        op = layout.operator(
+            ZENU_OT_action_selector_item_operations.bl_idname, text=name, icon=icon, depress=active)
+        op.action_name = self.action_name
+        op.action = action
 
     def draw_settings(self, layout: bpy.types.UILayout):
         context = bpy.context
@@ -266,6 +338,7 @@ class ZENU_PT_action_selector(BasePanel):
 
         actions: bpy.types.CollectionProperty = scene.zenu_action_selector
         action_index: int = scene.zenu_action_selector_active
+
         try:
             action_active: AC_Action = actions[action_index]
         except IndexError:
@@ -279,20 +352,33 @@ class ZENU_PT_action_selector(BasePanel):
             return
 
         col.prop(action_active.action, 'name', text='')
+        row = col.row(align=True)
+        row.prop(action_active, 'start', text='')
+        row.prop(action_active, 'end', text='')
+        
+        main_col = layout.column(align=True)
 
-        col = layout.column(align=True)
-        op = col.operator(
-            ZENU_OT_action_selector_item_operations.bl_idname, text='Select')
-        op.action_name = action_active.action.name
-        op.action = 'SELECT'
+        row = main_col.row(align=True)
+        row.scale_y = 2
+        self.action_name = action_active.action.name
 
-        op = col.operator(
-            ZENU_OT_action_selector_item_operations.bl_idname, text='Rename To Action Name')
+        self.draw_set_op(row, name='Select',
+                         icon='RESTRICT_SELECT_OFF', action='SELECT')
+        self.draw_set_op(row, name='Rename', icon='GREASEPENCIL',
+                         action='RENAME_TO_ACTION_NAME')
 
-        op.action_name = action_active.action.name
-        op.action = 'RENAME_TO_ACTION_NAME'
+        is_isolate = False
 
-        op = col.operator(ZENU_OT_copy_nla_strip_settings.bl_idname)
+        try:
+            obj = context.selected_objects[0]
+            is_isolate = obj.zenu_is_isolate
+        except IndexError:
+            pass
+
+        self.draw_set_op(row, name='Isolate',
+                         icon='OBJECT_HIDDEN', action='ISOLATE', active=is_isolate)
+
+        op = main_col.operator(ZENU_OT_copy_nla_strip_settings.bl_idname)
         op.action_name = action_active.action.name
 
     def draw_op(self, layout: bpy.types.UILayout, action: str, icon: str):
@@ -338,7 +424,8 @@ reg, unreg = bpy.utils.register_classes_factory((
 
 def register():
     reg()
-
+    bpy.types.Object.zenu_is_isolate = bpy.props.BoolProperty(
+        default=False, options={'ANIMATABLE'})
     bpy.types.Scene.zenu_action_selector = bpy.props.CollectionProperty(
         type=AC_Action)
     bpy.types.Scene.zenu_action_selector_active = bpy.props.IntProperty()
